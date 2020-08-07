6f00b661bb7371920a6f86427cb28a06ae74a1d1
Santiago Villavicencio <villavicencioandrs@gmail.com>
2020-08-06 20:30:22 -0500

    Creacion y modificacion.
    Creacion de la clase ConnecionDB (falta agregar el nombre de la BD) y falta terminar el metodo de Login

---
+++ ModellierungProjekt/src/Controlador/ConnectionDB.java
@@ -0,0 +1,31 @@
+package Controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Santiago Villavicencio villavicencioandrs@gmail.com
+ */
+public class ConnectionDB {
+
+    private final String URL = "jdbc:mysql://localhost/", USERNAME = "root", PASSWORD = "";
+
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+}
--- ModellierungProjekt/src/Controlador/Login.java
+++ ModellierungProjekt/src/Controlador/Login.java
@@ -1,5 +1,11 @@
 package Controlador;
 
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -8,16 +14,34 @@ import javax.swing.JOptionPane;
  */
 public class Login {
 
-    //ingresar
-    public static boolean ingresar(String usuario, String password) {
-        if(esUsuarioVacio(usuario)){
+    public Connection conn = ConnectionDB.getConnection();
+
+    // Ingresar
+    public boolean ingresar(String usuario, String password) {
+        if (esUsuarioVacio(usuario)) {
             JOptionPane.showMessageDialog(null, "El campo de Usuario se encuentra vacío.");
             return false;
         }
-        if(esPasswordVacio(password)){
+        if (esPasswordVacio(password)) {
             JOptionPane.showMessageDialog(null, "La Contraseña se encuentra vacía.");
             return false;
         }
+
+        try {
+            // Seleccionar usuario y contraseña
+            String usuarioDB, contraseña;
+            String sqlUsername = "SELECT * FROM --- WHERE ;";
+            
+            PreparedStatement ps = conn.prepareStatement(sqlUsername);
+            ResultSet rs = ps.executeQuery();
+            if (true) {
+
+            }
+            String sqlPassword = "SELECT * FROM --- WHERE ;";
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         return false;
     }
 
